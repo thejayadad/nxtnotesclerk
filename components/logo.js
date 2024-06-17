a544f65d9cfea62cb6dc46c68ff6a6ee3de40abf
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-06-16 21:59:47 -0400

    update

--- components/logo.js
+++ components/logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     className="mb-2 flex h-20 items-end justify-start rounded-md bg-primary p-4 md:h-40"
     href={'/'}
     >
-      <span className='text-brown-900'>Thought</span><span className='text-orange-900'>Track</span>
+      <span className='text-brown-900'>Thought</span><span className='text-orange-900'>Tracker</span>
     </Link> 
   )
 }
